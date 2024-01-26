813f1a1b48ed83c5aa791ea196ad00e579ef442c
natsaimasamvu <ctnatsai@gmail.com>
2024-01-26 13:46:28 +0100

    delta_lake_3_tier_architechture

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#export PYSPARK_DRIVER_PYTHON=jupyter
+PYSPARK_DRIVER_PYTHON=python
+export PYSPARK_DRIVER_PYTHON_OPTS='lab --ip=0.0.0.0'
+
+$SPARK_HOME/bin/pyspark --packages io.delta:${DELTA_PACKAGE_VERSION} \
+--conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" \
+--conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+
+curl -O https://jdbc.postgresql.org/download/postgresql-42.7.0.jar
+
+
